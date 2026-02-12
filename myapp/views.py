5504c2601fecd76d83af1100f7681a5bf9d872b4
Amanuel <amanuelwendimu19@gmail.com>
2026-02-12 22:18:19 +0300

    add Authentication System

---
+++ myapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "myapp/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "myapp/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+@login_required
+def profile_view(request):
+    return render(request, "myapp/profile.html")
+
+    
